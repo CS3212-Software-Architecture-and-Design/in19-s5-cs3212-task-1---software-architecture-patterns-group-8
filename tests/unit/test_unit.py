8c26b0e29a9ee46936d1a9597582d22f303a3dbd
Vinuhans <vinuhans123@gmail.com>
2022-09-09 19:57:31 +0530

    vinul-test_unit

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -11,13 +11,61 @@ class TestUnit(unittest.TestCase):
         result = Add(1, 2)
         self.assertEqual(str(result), '(1 Add 2)')
 
+        result = Add(0, 0)
+        self.assertEqual(str(result), '(0 Add 0)')
+
+        result = Add(-7, -8)
+        self.assertEqual(str(result), '(-7 Add -8)')
+
+        result = Add(0, 5)
+        self.assertEqual(str(result), '(0 Add 5)')
+
+        result = Add(0, -1)
+        self.assertEqual(str(result), '(0 Add -1)')
+
+        result = Add(5, 0)
+        self.assertEqual(str(result), '(5 Add 0)')
+
+        result = Add(-5, 0)
+        self.assertEqual(str(result), '(-5 Add 0)')
+
+        result = Add(-5, 8)
+        self.assertEqual(str(result), '(-5 Add 8)')
+
     def test_subtract(self):
         result = Subtract(7, 3)
         self.assertEqual(str(result), '(7 Subtract 3)')
 
+        result = Subtract(7, -3)
+        self.assertEqual(str(result), '(7 Subtract -3)')
+
+        result = Subtract(7, 0)
+        self.assertEqual(str(result), '(7 Subtract 0)')
+
+        result = Subtract(-7, 0)
+        self.assertEqual(str(result), '(-7 Subtract 0)')
+
+        result = Subtract(0, 3)
+        self.assertEqual(str(result), '(0 Subtract 3)')
+
+        result = Subtract(-3, 3)
+        self.assertEqual(str(result), '(-3 Subtract 3)')
+
+        result = Subtract(-3, 7)
+        self.assertEqual(str(result), '(-3 Subtract 7)')
+
+        result = Subtract(0, 0)
+        self.assertEqual(str(result), '(0 Subtract 0)')
+
     def test_number(self):
         result = Number('5')
         self.assertEqual(str(result), '5')
+
+        result = Number('-5')
+        self.assertEqual(str(result), '-5')
+
+        result = Number('0')
+        self.assertEqual(str(result), '0')
 
     def test_conversion_context(self):
         result = ConversionContext.parse_input('1 + 2 - 3')
