3dcbfae2160eb38a0a058892a3381a080adfe8fb
Akash <akash_tharuka@yahoo.com>
2022-09-09 14:11:01 +0530

    timeout integration testing

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -13,3 +13,11 @@ class TestUnit(unittest.TestCase):
         expression = "1 + 2 + 0 - 8"
         result = ConversionContext.parse_input(expression)
         self.assertEqual(str(result.interpret()), '-5')
+
+    def test_timeout(self):
+        expression = "1342 - 435 + 321 - 0 - 345 - 901 + 43 + 891 - 8945 + 7645 + 894 - 476 + 9 - 21"
+        result = ConversionContext.parse_input(expression)
+        self.assertEqual(str(result.interpret()), '22')
+
+if __name__ == '__main__':
+    unittest.main()
