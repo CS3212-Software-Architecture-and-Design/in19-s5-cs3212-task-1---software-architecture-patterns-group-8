73038937f24ff66ce4353da9b9ddf39189bc4a9e
Akash <akash_tharuka@yahoo.com>
2022-09-10 09:56:58 +0530

    rollback some changes

--- interpreter/number.py
+++ interpreter/number.py
@@ -7,9 +7,6 @@ class Number(Expression):
     def __init__(self, value):
         self.value = int(value)
 
-    def get_value(self):
-        return self.value
-
     def interpret(self):
         return self.value
 
